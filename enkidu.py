4f8d7e81d627b310328c514107d7317694a1c6ca
sajarindider <sajarindider@gmail.com>
2020-02-02 21:16:15 -0500

    doc: updated readme.md

--- enkidu.py
+++ enkidu.py
@@ -1,5 +1,6 @@
 # Author: Sajarin Dider
-# from fpdf import FPDF
+# Description: This program reads in a pdf file, converts it to text and then applies LexRank to summarize the text. It outputs the summarization in a separte text file 
+
 import pdfminer
 import pdfminer.high_level
 from sumy.summarizers.lex_rank import LexRankSummarizer
@@ -8,25 +9,17 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
-# output=FPDF()
+#open the file
 filename = './c.pdf'
-# pdf_reader = PyPDF2.PdfFileReader(pdf)
 
-
-# count = 0
-# text = ""
-# page_num = pdf_reader.numPages
-
-# while count < page_num: 
-#     page = pdf_reader.getPage(count)
-#     count += 1
-#     text += page.extractText()
-
+#extract text from filename
 text = pdfminer.high_level.extract_text(filename, password='', page_numbers=None, maxpages=10, caching=True, codec='utf-8', laparams=None)
 
+#test if text is empty
 if text == "":
     print("No text extracted")
 
+#create text files for extracted text and summarized output file
 text_file = open("example.txt", "w", encoding="utf-8", errors="ignore")
 summary = open('summary.txt', "a")
 
@@ -36,15 +29,8 @@ parser = PlaintextParser.from_file(text_file.name, Tokenizer('english'))
 stemmer = Stemmer("english")
 
 summarizer = LexRankSummarizer(stemmer)
-# output.add_page()
-# output.set_font('Arial')
 
-# height = 10
-# width = 40 
 for sentence in summarizer(parser.document, 20):
 
     print(sentence, "\n")
-    # output.cell(width,height, str(sentence))
-    # width += 10
     summary.write(str(sentence))
-# output.output('output.pdf','F')
